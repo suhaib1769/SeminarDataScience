6bf81ee90459e0243627ec4b521fce1e49adb080
suhaib1769 <suhaib.basir@gmail.com>
2023-06-15 18:42:10 +0200

    final touches to part 1

--- Assignment_3/lab3_pca_2.m
+++ Assignment_3/lab3_pca_2.m
@@ -1,7 +1,6 @@
 % get dataset 
-data1 = readtable('google_review_ratings.csv', VariableNamingRule='preserve');
+data1 = readtable('data.csv', VariableNamingRule='preserve');
 data1(:,end) = [];
-data1(:,1) = [];
 data1_matrix = table2array(data1);
 disp(size(data1_matrix));
 
@@ -14,49 +13,59 @@ mu = mean(data1_matrix);
 data_centered = data1_matrix - mu;
 
 % Step 3: Compute the covariance matrix
-n = size(data_centered, 1);
-% C = (data_centered' * data_centered) / (n - 1);
-C = cov(data_centered);
+[n, m] = size(data_centered); % n = number of observations, m = number of variables
+% normalization_factor = n - 1; % For unbiased estimate
+% C = (data_centered' * data_centered) ./ normalization_factor;
+C = (1/m)*(data_centered*data_centered');
+% C = cov(data_centered);
 
 % Step 4: Compute eigenvalues and eigenvectors of the covariance matrix
 [V, D] = eig(C);
 
-% Sort the eigenvalues in descending order and get the indices
-[eigValues, order] = sort(diag(D), 'descend');
-
-% Sort the eigenvectors based on the order of eigenvalues
-eigVectors = V(:, order);
-
-% Project the original data onto the PCA space
-data_pca = data_centered * eigVectors;
-
 % Display the eigenvalues and eigenvectors
-disp('Eigenvalues:')
-disp(diag(D))
-disp('Eigenvectors:')
-disp(V);
-
-% Step 5: Extract eigenvalues from matrix D
-eigenvalues = diag(D);
-
-% Step 6: Calculate proportion of variance explained
-variance_explained = eigenvalues / sum(eigenvalues);
-
-% Step 7: Compute cumulative proportion of variance explained
-cumulative_variance_explained = cumsum(variance_explained);
+% disp('Covariance')
+% disp('Eigenvalues:')
+% disp(diag(D))
+% disp('Eigenvectors:')
+% disp(V);
 
 elapsed_time = toc;
 disp(['Elapsed Time: ' num2str(elapsed_time) ' seconds']);
 
-% Step 8: Plot the scree plot
+% Extract eigenvalues from matrix D
+eigenvalues = diag(D);
+
+% Calculate proportion of variance explained
+variance_explained = eigenvalues / sum(eigenvalues);
+
+% Compute cumulative proportion of variance explained
+cumulative_variance_explained = cumsum(variance_explained);
+
+% % Plot the scree plot
+% figure
+% plot(1:numel(eigenvalues), variance_explained, 'bo-')
+% hold on
+% plot(1:numel(eigenvalues), cumulative_variance_explained, 'ro-')
+% xlabel('Principal Component')
+% ylabel('Proportion of Variance Explained')
+% title('Scree Plot - PCA with covariance matrix')
+% legend('Variance Explained', 'Cumulative Variance Explained')
+% grid on
+
+[d,ind] = sort(diag(D),'descend'); % sort eigenvalues in descending order
+V = V(:,ind); % reorder columns of V (eigenvectors) accordingly
+
+i = 5; % adjust this value to the desired number of dimensions
+V_i = V(:, 1:i); % take the first i eigenvectors
+
+% Project the centered data onto the new space
+data_projected = data_centered'*V_i ; % transpose data_centered to match dimensions
+
 figure
-plot(1:numel(eigenvalues), variance_explained, 'bo-')
-hold on
-plot(1:numel(eigenvalues), cumulative_variance_explained, 'ro-')
-xlabel('Principal Component')
-ylabel('Proportion of Variance Explained')
-title('Scree Plot')
-legend('Variance Explained', 'Cumulative Variance Explained')
+scatter(data_projected(:, 1), data_projected(:, 2), 'b', 'filled')
+xlabel('Principal Component 1')
+ylabel('Principal Component 2')
+title('Projected Data Visualization')
 grid on
 
 %% PCA with Gram matrix
@@ -66,32 +75,73 @@ mu2 = mean(data1_matrix);
 
 % Step 2: Compute the centered points
 data_centered2 = data1_matrix - mu2;
+[n, m] = size(data_centered2); % n = number of observations, m = number of variables
 
 % Step 3: Compute the Gram matrix
 G = data_centered2' * data_centered2;
-
 % Step 4: Compute eigenvalues and eigenvectors of the Gram matrix
 [V2, D2] = eig(G);
 
-% Sort the eigenvalues in descending order and get the indices
-[eigValues2, order] = sort(diag(D2), 'descend');
+% disp('Gram')
+disp('Eigenvalues:')
+disp(diag(D2))
+% disp('Eigenvectors:')
+% disp(V2);
 
-% Sort the eigenvectors based on the order of eigenvalues
-eigVectors2 = V2(:, order);
+elapsed_time = toc;
+disp(['Elapsed Time: ' num2str(elapsed_time) ' seconds']);
+
+% Extract eigenvalues from matrix D
+eigenvalues = diag(D2);
+
+% Calculate proportion of variance explained
+variance_explained = eigenvalues / sum(eigenvalues);
+
+% Compute cumulative proportion of variance explained
+cumulative_variance_explained = cumsum(variance_explained);
+
+% Plot the scree plot
+figure
+plot(1:numel(eigenvalues), variance_explained, 'bo-')
+hold on
+plot(1:numel(eigenvalues), cumulative_variance_explained, 'ro-')
+xlabel('Principal Component')
+ylabel('Proportion of Variance Explained')
+title('Scree Plot - PCA with Gram matrix')
+legend('Variance Explained', 'Cumulative Variance Explained')
+grid on
 
 % Step 5: Compute the basis vectors of the affine spaces
-basis_vectors = eigVectors2 * data_centered2';
+basis_vectors = data_centered2* V2;
 basis_vectors = basis_vectors ./ vecnorm(basis_vectors);
 
-% Display the basis vectors
-disp('Basis vectors:')
-disp(basis_vectors)
+
+[d2,ind2] = sort(diag(D2),'descend'); % sort eigenvalues in descending order
+V2 = V2(:,ind2); % reorder columns of V2 (eigenvectors) accordingly
+
+i = 5; % adjust this value to the desired number of dimensions
+V2_i = basis_vectors(:, 1:i); % take the first i basis vectors
+
+% Project the centered data onto the new space
+data_projected2 = V2_i' * data_centered2; % transpose data_centered2 to match dimensions
+
+
+%% Comparison of PCA with MATLAB's built-in pca function 
+tic
+% [coeff,score,latent,~,explained] = pca(data1_matrix);
+[coeff,score,latent,~,explained] = pca(data1_matrix);
+
+% sort in ascending order
+[latent, order] = sort(latent, 'ascend'); % sort eigenvalues
+coeff = coeff(:,order); % rearrange eigenvectors
+explained = explained(order); % rearrange explained variance
+score = score(:,order); % rearrange scores
 
 elapsed_time = toc;
 disp(['Elapsed Time: ' num2str(elapsed_time) ' seconds']);
 
 % Step 6: Extract eigenvalues from matrix D
-eigenvalues = diag(D);
+eigenvalues = (latent);
 
 % Step 7: Calculate proportion of variance explained
 variance_explained = eigenvalues / sum(eigenvalues);
@@ -106,22 +156,24 @@ hold on
 plot(1:numel(eigenvalues), cumulative_variance_explained, 'ro-')
 xlabel('Principal Component')
 ylabel('Proportion of Variance Explained')
-title('Scree Plot')
+title('Scree Plot - in built PCA')
 legend('Variance Explained', 'Cumulative Variance Explained')
 grid on
 
-%% Comparison of PCA with MATLAB's built-in pca function 
-tic
-[coeff,score,latent,~,explained] = pca(data1_matrix);
+i = 5; % adjust this value to the desired number of dimensions
+coeff_i = coeff(:, 1:i); % take the first i principal components
 
-% Display the eigenvalues and eigenvectors
-disp('Eigenvalues:')
+% Project the original data onto the new space
+data_centered3 = data1_matrix - mean(data1_matrix); % center the original data
+data_projected3 = coeff_i' * data_centered3'; % transpose data_centered3 to match dimensions
+
+% Display the eigenvalues (or explained variances) for each method
+disp('Eigenvalues for method 1:')
+disp(diag(D))
+disp('Eigenvalues for method 2:')
+disp(diag(D2))
+disp('Eigenvalues for method 3:')
 disp(latent)
-disp('Eigenvectors:')
-disp(coeff);
-
-elapsed_time = toc;
-disp(['Elapsed Time: ' num2str(elapsed_time) ' seconds']);
 
 %% Get storage data
 whos;
